3776af93cba09dcebaa7e7f6144f63b50c846aca
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-04-05 14:48:37 -0500

    Set up for simple web server

--- HonestPoker/Startup.cs
+++ HonestPoker/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -15,20 +16,31 @@ namespace HonestPoker
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            // Redirect any non-API calls to the Angular applicat so our application can handle the routing
+            app.Use(async (context, next) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                    !Path.HasExtension(context.Request.Path.Value) &&
+                    !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
             });
+            // Configures application for usage as API w/ default route of '/api/[Controller]'
+            app.UseMvcWithDefaultRoute();
+
+            // Configures the application to serve the index.html file from /wwwroot when 
+            // you access the server from a web browser
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
